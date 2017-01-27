bb31c9bbd4ea192ee7d5162f3164f1177c624cb2
sminux <espin55@yandex.ru>
2017-01-27 12:30:09 +0300

    v0.1

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 '''
 Created on 17 янв. 2017 г.
 
-@author: Сергей
+@author: sminux
 '''
 
 import telebot
 import constants
 import random
-import urllib
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
@@ -16,8 +15,6 @@ bot = telebot.TeleBot(constants.token)
 upd = bot.get_updates()
 print("\n____________")
 print(bot.get_me())
-
-#group_list = {"Виноградова В М":1, "Волнухин В В":2, "Грызлов П Д":3, "Даруткин К А":4, "Ернеев Э М":5, "Карапетян Г Л":6, "Легкая А С":7, "Литвак А П":8, "Марченков С Д":9, "Прохоров М В":10, "Прохоров Д Д":11, "Саморуков А М":12, "Сапронов Е О":13, "Свойкина А Г":14, "Синотова О В":15, "Сундугеев Н А":16, "Шапкин А С":17, "Яснов А И":18}
 
 def log(message, answer):
     print("\n____________")
@@ -40,77 +37,78 @@ def get_week():
 def get_shedule_ch():
     url_bmstu = urlopen(constants.url_schedule).read()
     soup = BeautifulSoup(url_bmstu, "lxml")
-    i = 0
     answer_ch = 'ЧС 📚\n'
     divs = soup.find_all('div', attrs = {"class":"col-md-6 hidden-xs"})
     for div in divs:
+        answer_ch  += '📋'
         trs = div.find_all('tr')
         for tr in trs:
             td_time = tr.find_all('td', attrs={"class":"bg-grey text-nowrap"})
             lesson_success = tr.find_all('td', attrs={"class":"text-success"})
-            lesson_info = tr.find_all('td', attrs={"class":"text-info"})
             lesson_ordinary = tr.find_all('td', attrs={"colspan":"2"})
             days = tr.find_all('strong')
             time = [i.text for i in td_time]
             lesson_cheslitel = [les.text for les in lesson_success]
-            lesson_znamenatel = [les.text for les in lesson_info]
             lesson_ordinary = [les.text for les in lesson_ordinary]
             day = [day.text for day in days]
-            
-            list = (day + time + lesson_cheslitel + lesson_ordinary).__str__()
-            answer_ch += '<b>{}</b>,\n {}'.format(day.__str__(), list)#day.__str__() + '\n' + list
-            
-            i = i + 2
-        answer_ch += '\n📋'
+            answer_ch += '<b>{}</b>\n<b>{}</b> {}{}'.format(day,
+                                                            time,
+                                                            lesson_cheslitel,
+                                                            lesson_ordinary)
+
+        answer_ch += '\n'
     answer_ch = answer_ch.replace('xa0', '')
-    answer_ch = answer_ch.replace('[', '') 
+    answer_ch = answer_ch.replace('[', '')
+    answer_ch = answer_ch.replace('\\', ' ')
     answer_ch = answer_ch.replace(']', '')
-    answer_ch = answer_ch.replace("'", '')   
+    answer_ch = answer_ch.replace("'", '')
+    answer_ch = answer_ch.replace("<b></b> <b></b>", '')
     answer_ch = answer_ch.replace("\n\n", '')        
     return  answer_ch
  
 def get_shedule_zn():
     url_bmstu = urlopen(constants.url_schedule).read()
     soup = BeautifulSoup(url_bmstu, "lxml")
-    i = 0
+
     answer_zn = 'ЗН 📚\n'
     divs = soup.find_all('div', attrs = {"class":"col-md-6 hidden-sm hidden-md hidden-lg"})
     for div in divs:
+        answer_zn += '📋'
         trs = div.find_all('tr')
         for tr in trs:
             td_time = tr.find_all('td', attrs={"class":"bg-grey text-nowrap"})
-            lesson_success = tr.find_all('td', attrs={"class":"text-success"})
             lesson_info = tr.find_all('td', attrs={"class":"text-info"})
             lesson_ordinary = tr.find_all('td', attrs={"colspan":"2"})
             days = tr.find_all('strong')
             time = [i.text for i in td_time]
-            lesson_cheslitel = [les.text for les in lesson_success]
             lesson_znamenatel = [les.text for les in lesson_info]
             lesson_ordinary = [les.text for les in lesson_ordinary]
             day = [day.text for day in days]
-            
-            list = (time + lesson_znamenatel + lesson_ordinary).__str__()
-            answer_zn += day.__str__() + '\n' + list
-            
-            i = i + 2 
-        answer_zn += '\n📋'
+            answer_zn += '<b>{}</b>\n<b>{}</b> {}{}'.format(day,
+                                                            time,
+                                                            lesson_znamenatel,
+                                                            lesson_ordinary)
+
+        answer_zn += '\n'
     answer_zn = answer_zn.replace('xa0', '')
-    answer_zn = answer_zn.replace('[', '') 
+    answer_zn = answer_zn.replace('[', '')
+    answer_zn = answer_zn.replace('\\', ' ')
     answer_zn = answer_zn.replace(']', '')
-    answer_zn = answer_zn.replace("'", '')  
-    answer_zn = answer_zn.replace("\n\n", '')   
+    answer_zn = answer_zn.replace("'", '')
+    answer_zn = answer_zn.replace("<b></b> <b></b>", '')
+    answer_zn = answer_zn.replace("\n\n", '')
     return  answer_zn
-    
+
 @bot.message_handler(commands = ['help'])
 def handle_help(message):
-    bot.send_message(message.chat.id, 'Тебе никто не поможет 😈 \n\nP.S.\nКоманда "расписание" выводит расписание на текущую неделю\nЧтобы обратиться к порталу bmstu.wiki, пишите "Кто такой ФИО?"\nЧтобы узнать свой порядковый номер, запросите "список группы"\nТакже вы можете узнать "расписание на неделю" или только на "сегодня"\nА если Вы заблудились и не найдёте нужный кабинет, спросите "где находится [№ауд.]" ')
+    bot.send_message(message.chat.id, 'Тебе никто не поможет 😈 \n\nP.S.\nЧтобы обратиться к порталу bmstu.wiki, пишите "Кто такой ФИО?"\nЧтобы узнать свой порядковый номер, запросите "список группы"\nТакже вы можете узнать "расписание" на неделю\nКоманда "новости" предоставит последнюю новостную заметку\nВопросы и предложения пишите автору: @sminux')
         
 @bot.message_handler(commands = ['start'])
 def handle_start(message):
     user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
     user_markup.row('/start', '/stop')
     user_markup.row('список группы', 'диск', 'неделя (ч/з)')
-    user_markup.row('расписание', 'сегодня' )
+    user_markup.row('новости', 'расписание' )
     bot.send_message(message.from_user.id, "Чем могу помочь...", reply_markup=user_markup)
     
 @bot.message_handler(commands = ['stop'])
@@ -122,11 +120,11 @@ def handle_stop(message):
 def handle_text(message):
         answer = "Не понятно🤔  Попробуйте ещё раз."
         msg = message.text
-        if msg == 'диск' :
+        if msg == 'диск':
             answer = 'https://drive.google.com/open?id=0Bwpp4JuViU2KNWpic3loeGlPeHM'
             log(message, answer)
             bot.send_message(message.chat.id, answer)   
-        elif msg == 'неделя (ч/з)' :
+        elif msg == 'неделя (ч/з)':
             '''from datetime import date
             today = date.today()
             month = today.strftime("%b")
@@ -134,7 +132,7 @@ def handle_text(message):
             answer = get_week()
             log(message, answer)
             bot.send_message(message.chat.id, answer) 
-        elif msg == 'список группы' :
+        elif msg == 'список группы':
             answer = "список группы ИУ8-" + (constants.sem).__str__() + "2:\n"
             i = 0
             for student in constants.group_list:
@@ -142,25 +140,40 @@ def handle_text(message):
                 i = i + 1            
             log(message, answer)
             bot.send_message(message.chat.id, answer)   
-        elif msg == 'расписание' :
+        elif msg == 'расписание':
             if (get_week().find('числитель', 0)>=0):
                answer = get_shedule_ch()
             elif (get_week().find('знаменатель', 0)>=0): 
                 answer = get_shedule_zn()
             log(message, answer)
             bot.send_message(message.chat.id, answer, parse_mode='HTML')   
-        elif msg == 'сегодня' :
+        elif msg == 'новости':
             answer = 'Въ салонѣ тихо, пыльно и пусто.\nТолько въ одномъ изъ угловъ вышиваетъ гладью пожилая княжна.'
+
+            url_news = urlopen('https://vk.com/pr.bmstu').read()
+            soup = BeautifulSoup(url_news, "lxml")
+            news = soup.find('div', attrs={"class": "pi_text"})
+            answer = news.text
             log(message, answer)
             bot.send_message(message.chat.id, answer)              
         elif ((msg.find('кто такой', 0)>=0) | (msg.find('кто такая', 0)>=0) | (msg.find('Кто такой', 0)>=0) | (msg.find('Кто такая', 0)>=0)):
-            str = msg.replace('?', '')
+            str = msg.replace("?", '')
+            str = str.replace(',', '')
+            str = str.replace('.', '')
+            str = str.replace('/', '')
+            str = str.replace('&', '')
             str = str.replace('Кто', '')
             str = str.replace('кто', '')
             str = str.replace('такой', '') 
-            str = str.replace('такая', '') 
-            #функция поиска в wiki
-            answer = constants.wiki + str
+            str = str.replace('такая', '')
+            text = ''
+            fio = str.split(' ')
+            if len(fio) != 5:
+                text = 'Мне нужны полные данные [ФИО]. Повторите запрос.'
+            elif len(fio) == 5:
+                text = constants.wiki + fio[2].capitalize() + ',_' + fio[3].capitalize() + '_' + fio[4].capitalize()
+
+            answer = text
             log(message, answer)
             bot.send_message(message.chat.id, answer)  
         elif ((msg.find('студент', 0)>=0) | (msg.find('МГТУ', 0)>=0) | (msg.find('мгту', 0)>=0) | (msg.find('бауманк', 0)>=0) | (msg.find('сдаст', 0)>=0) | (msg.find('сдадим', 0)>=0)):
